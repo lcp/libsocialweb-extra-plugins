79e7c8ffe658e11fe183c0cbcaf717b13fa9ea30
Gary Ching-Pang Lin <chingpang@gmail.com>
2010-11-09 18:55:27 +0800

    [MySpace] Update copyright headers

--- services/myspace/myspace.h
+++ services/myspace/myspace.h
@@ -1,6 +1,7 @@
 /*
- * libsocialweb - social data store
+ * libsocialweb MySpace service support
  * Copyright (C) 2008 - 2009 Intel Corporation.
+ * Copyright (C) 2010 Novell, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU Lesser General Public License,
