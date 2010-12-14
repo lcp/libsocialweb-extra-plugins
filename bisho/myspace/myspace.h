8899424deb86a0cfcdf36928ed211339ee120998
Gary Ching-Pang Lin <chingpang@gmail.com>
2010-12-14 12:04:24 +0800

    bisho/myspace/myspace.h: Object renaming

--- bisho/myspace/myspace.h
+++ bisho/myspace/myspace.h
@@ -18,48 +18,48 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __BISHO_PANE_SINA_H__
-#define __BISHO_PANE_SINA_H__
+#ifndef __BISHO_PANE_MYSPACE_H__
+#define __BISHO_PANE_MYSPACE_H__
 
 #include <bisho/bisho-pane.h>
 
 G_BEGIN_DECLS
 
-#define BISHO_TYPE_PANE_SINA (bisho_pane_sina_get_type())
-#define BISHO_PANE_SINA(obj)                                           \
+#define BISHO_TYPE_PANE_MYSPACE (bisho_pane_myspace_get_type())
+#define BISHO_PANE_MYSPACE(obj)                                         \
    (G_TYPE_CHECK_INSTANCE_CAST ((obj),                                  \
-                                BISHO_TYPE_PANE_SINA,                  \
-                                BishoPaneSina))
-#define BISHO_PANE_SINA_CLASS(klass)                                   \
+                                BISHO_TYPE_PANE_MYSPACE,                \
+                                BishoPaneMySpace))
+#define BISHO_PANE_MYSPACE_CLASS(klass)                                 \
    (G_TYPE_CHECK_CLASS_CAST ((klass),                                   \
-                             BISHO_TYPE_PANE_SINA,                     \
-                             BishoPaneSinaClass))
-#define BISHO_IS_PANE_SINA(obj)                                        \
+                             BISHO_TYPE_PANE_MYSPACE,                   \
+                             BishoPaneMySpaceClass))
+#define BISHO_IS_PANE_MYSPACE(obj)                                      \
    (G_TYPE_CHECK_INSTANCE_TYPE ((obj),                                  \
-                                BISHO_TYPE_PANE_SINA))
-#define BISHO_IS_PANE_SINA_CLASS(klass)                                \
+                                BISHO_TYPE_PANE_MYSPACE))
+#define BISHO_IS_PANE_MYSPACE_CLASS(klass)                              \
    (G_TYPE_CHECK_CLASS_TYPE ((klass),                                   \
-                             BISHO_TYPE_PANE_SINA))
-#define BISHO_PANE_SINA_GET_CLASS(obj)                                 \
+                             BISHO_TYPE_PANE_MYSPACE))
+#define BISHO_PANE_MYSPACE_GET_CLASS(obj)                               \
    (G_TYPE_INSTANCE_GET_CLASS ((obj),                                   \
-                               BISHO_TYPE_PANE_SINA,                   \
-                               BishoPaneSinaClass))
+                               BISHO_TYPE_PANE_MYSPACE,                 \
+                               BishoPaneMySpaceClass))
 
-typedef struct _BishoPaneSinaPrivate BishoPaneSinaPrivate;
-typedef struct _BishoPaneSina        BishoPaneSina;
-typedef struct _BishoPaneSinaClass   BishoPaneSinaClass;
+typedef struct _BishoPaneMySpacePrivate BishoPaneMySpacePrivate;
+typedef struct _BishoPaneMySpace        BishoPaneMySpace;
+typedef struct _BishoPaneMySpaceClass   BishoPaneMySpaceClass;
 
-struct _BishoPaneSina {
+struct _BishoPaneMySpace {
   BishoPane parent;
-  BishoPaneSinaPrivate *priv;
+  BishoPaneMySpacePrivate *priv;
 };
 
-struct _BishoPaneSinaClass {
+struct _BishoPaneMySpaceClass {
   BishoPaneClass parent_class;
 };
 
-GType bisho_pane_sina_get_type (void) G_GNUC_CONST;
+GType bisho_pane_myspace_get_type (void) G_GNUC_CONST;
 
 G_END_DECLS
 
-#endif /* __BISHO_PANE_SINA_H__ */
+#endif /* __BISHO_PANE_MYSPACE_H__ */
